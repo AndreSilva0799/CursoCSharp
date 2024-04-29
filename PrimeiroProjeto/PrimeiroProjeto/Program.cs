0f685bccb34dd86fc5cb224deb5ced081c8ef8a5
Andre Victor <andrevictor2102@hotmail.com>
2024-04-29 18:07:58 -0300

    Criando repositorio do curso de C#

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte x = 100;
+
+            Console.WriteLine("sbyte " + x);
+
+            byte n1 = 126;
+
+            Console.WriteLine("byte " + n1);
+
+            int n2 = 1000;
+            Console.WriteLine("int " + n2);
+
+            int n3 = 2147443648;
+
+            Console.WriteLine("int " + n3);
+
+            long n4 = 2147443648L;
+
+            Console.WriteLine("long " + n4);
+
+            bool completo = false;
+
+            Console.WriteLine("bool " + completo);
+
+            char genero = 'F';
+
+            Console.WriteLine("char " + genero);
+
+        }
+    }
+}
