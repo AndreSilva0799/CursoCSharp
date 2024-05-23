8c40a0ec328e12fd42694f0596dbfa3777145780
Andre Victor <andrevictor2102@hotmail.com>
2024-05-23 07:54:16 -0300

    Classes

--- Course/Course/Triangulo.cs
+++ Course/Course/Triangulo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,12 @@ namespace Course
         public double B;
         public double C;
 
-
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+           return Math.Sqrt(p * (p - A) * (p-B) * (p - C));
+            
+        }
 
     }
 }
